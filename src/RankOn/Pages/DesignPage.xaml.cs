36244355d170e8efcd4180768fe3faac0e02dbb4
SINSEOL1 <109088262+SINSEOL1@users.noreply.github.com>
2026-09-19 01:58:44 +0900

    feat: add design page logic

---
+++ src/RankOn/Pages/DesignPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace RankOn.Pages;
+
+public partial class DesignPage : UserControl
+{
+    public DesignPage()
+    {
+        InitializeComponent();
+    }
+}
