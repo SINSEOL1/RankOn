468dc388e9677d1231e05ba390172ac969e6a1a3
SINSEOL1 <109088262+SINSEOL1@users.noreply.github.com>
2026-09-19 01:59:03 +0900

    feat: add profiles page logic

---
+++ src/RankOn/Pages/ProfilesPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace RankOn.Pages;
+
+public partial class ProfilesPage : UserControl
+{
+    public ProfilesPage()
+    {
+        InitializeComponent();
+    }
+}
