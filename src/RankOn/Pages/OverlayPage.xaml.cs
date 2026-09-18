7650a22c75b372f02f1e0e82c6f690b00671fd72
SINSEOL1 <109088262+SINSEOL1@users.noreply.github.com>
2026-09-19 01:58:38 +0900

    feat: add pc overlay page logic

---
+++ src/RankOn/Pages/OverlayPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace RankOn.Pages;
+
+public partial class OverlayPage : UserControl
+{
+    public OverlayPage()
+    {
+        InitializeComponent();
+    }
+}
